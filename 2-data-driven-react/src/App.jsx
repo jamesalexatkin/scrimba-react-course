159867414b1d09be86ad7a83f0aa465f6783d5c3
James Atkin <curlyw42@gmail.com>
2025-07-06 18:12:38 +0100

    pass props as spread

--- 2-data-driven-react/src/App.jsx
+++ 2-data-driven-react/src/App.jsx
@@ -4,17 +4,7 @@ import data from "./data";
 
 function App() {
   const entries = data.map(function (e) {
-    return (
-      <Entry
-        key={e.id}
-        img={e.img}
-        title={e.title}
-        country={e.country}
-        googleMapsLink={e.googleMapsLink}
-        dates={e.dates}
-        text={e.text}
-      ></Entry>
-    );
+    return <Entry key={e.id} {...e}></Entry>;
   });
   return (
     <>
