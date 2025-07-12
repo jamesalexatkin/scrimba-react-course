317f2a7b515479c6954e639a64bc238c2227d570
James Atkin <curlyw42@gmail.com>
2025-07-12 14:14:06 +0100

    start third section

---
+++ 3-react-state/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <>
+      <header>
+        <img src="/Chef Claude Icon.png"></img>
+        <h1>Chef Claude</h1>
+      </header>
+    </>
+  );
+}
